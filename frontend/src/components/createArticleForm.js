2c28474b64c051bd6d46acd6e6dff37c4cd48577
Bitarães <igor_oliveira02@hotmail.com>
2021-05-15 14:24:53 -0300

    add article send function

--- frontend/src/components/createArticleForm.js
+++ frontend/src/components/createArticleForm.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Container from './container';
 
-export default function CreatePost() {
+export default function CreatePost(props) {
     return (
         <Container>
             <div className="create-article-form">
                 <h2>Criar um artigo</h2>
-                <input type="text" name="title" id="create-title" placeholder="Título do artigo"></input>
-                <textarea name="description" id="create-description" placeholder="Descrição do artigo"></textarea>
-                <textarea name="article" id="create-article" placeholder="Artigo"></textarea>
+                <input type="text" name="title" id="create-title" placeholder="Título do artigo" onChange={props.onChange}></input>
+                <textarea name="description" id="create-description" placeholder="Descrição do artigo" onChange={props.onChange}></textarea>
+                <textarea name="articleBody" id="create-article" placeholder="Artigo" onChange={props.onChange}></textarea>
                 <label htmlFor="create-article-img">Faça upload de uma imagem para o artigo</label>
-                <input type="file" id="create-article-img" name="article-img" accept="image/png, image/jpeg"></input>
-                <input type="submit" value="Criar Artigo"></input>
+                <input type="file" id="create-article-img" name="article-img" accept="image/png, image/jpeg" onChange={props.onChange}></input>
+                <input type="submit" value="Criar Artigo" onClick={props.onClick}></input>
             </div>
         </Container>
     )
--- frontend/src/createArticlePage.js
+++ frontend/src/createArticlePage.js
@@ -4,10 +4,27 @@ import Footer from './components/footer';
 import Header from './components/header';
 
 export default function CreateArticle() {
+    const article = {}
+    function sendArticle() {
+        const { title, description, articleBody } = article;
+        const date = new Date();
+        if (!title || !description || !articleBody) {
+            return alert("Todos os campos devem ser preenchidos");
+        };
+        const postDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    };
+
+    function setArticle(e) {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+        article.[name] = value;
+    };
+
     return (
         <>
             <Header />
-            <CreatePost></CreatePost>
+            <CreatePost onClick={sendArticle} onChange={setArticle}></CreatePost>
             <Footer />
         </>
     )
