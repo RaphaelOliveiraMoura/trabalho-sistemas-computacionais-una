fcf88e814fea5befa9312f591f17bfd37e15c4f1
Igor <igor_oliveira02@hotmail.com>
2021-05-31 16:44:02 -0300

    add pagination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,16 +8,23 @@ import Footer from './components/footer';
 
 export default function App() {
   const [posts, setPosts] = useState([]);
+  const [pagesToLoad, setPagesToLoad] = useState(5);
+  const [totalPages, setTotalPages] = useState(0);
   useEffect(() => {
     mountPage()
-  }, [])
+  }, [pagesToLoad])
 
   async function mountPage() {
     const baseUrl = "http://54.234.248.140:3333/posts";
-    const headers = { "authorization": "Bearer " + localStorage.getItem("tkn") }
+    const headers = { "authorization": "Bearer " + localStorage.getItem("tkn") };
     const pagesRequest = await fetch(baseUrl, { method: "GET", headers: headers });
     const pagesResponse = await pagesRequest.json();
-    setPosts(pagesResponse.reverse());
+    setTotalPages(pagesResponse.length);
+    setPosts(pagesResponse.reverse().slice(0, pagesToLoad));
+  }
+
+  function loadPages() {
+    setPagesToLoad(parseInt(pagesToLoad + 5));
   }
 
   return (
@@ -29,6 +36,12 @@ export default function App() {
             {
               !posts ? mountPage() : posts.map(atual => <PostCard key={atual.id} post={atual} />)
             }
+            {
+              pagesToLoad < totalPages ? <div id="reload">
+                <button onClick={loadPages}>Carregar mais postagens</button>
+              </div> : ""
+            }
+
           </Posts>
         </Content>
       </Container>
